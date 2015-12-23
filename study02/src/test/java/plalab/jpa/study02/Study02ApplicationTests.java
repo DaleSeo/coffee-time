2fc576867c80304858d3ad2ce41e69f6478b56d4
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2015-12-23 10:26:40 +0900

    study02 project created.

---
+++ study02/src/test/java/plalab/jpa/study02/Study02ApplicationTests.java
@@ -0,0 +1,18 @@
+package plalab.jpa.study02;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Study02Application.class)
+@WebAppConfiguration
+public class Study02ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
