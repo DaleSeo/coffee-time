b31812fc317409905e80c4d8efea7ee4077478be
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-01-16 11:56:04 +0900

    머지 과정 중 어디선가 누락된 findName 메서도 추가

--- study02/src/main/java/plalab/jpa/study02/repository/CategoryRepository.java
+++ study02/src/main/java/plalab/jpa/study02/repository/CategoryRepository.java
@@ -2,9 +2,15 @@ package plalab.jpa.study02.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import plalab.jpa.study02.domain.Category;
+import plalab.jpa.study02.domain.Item;
+
+import java.util.List;
 
 /**
  * Updated on : 2015-12-23. Updated by : 양해엽, SK Planet.
  */
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findByName(String name);
+
 }
