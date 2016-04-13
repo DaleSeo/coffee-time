49234fa78610d62b3d38e1ded873736f59c0f32a
서대영/Store기술개발팀/SKP <dale.seo@sk.com>
2016-04-13 13:38:38 +0900

    뷰 이름을 Path Variable로 처리하여 웹컨트롤러 단순화

--- coffee-time/src/main/java/time/coffee/web/MemberWebController.java
+++ coffee-time/src/main/java/time/coffee/web/MemberWebController.java
@@ -1,6 +1,7 @@
 package time.coffee.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,24 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/web/member", method = RequestMethod.GET)
 public class MemberWebController {
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String hello() {
-		return "member/list";
-	}
-
-	@RequestMapping(value = "/create", method = RequestMethod.GET)
-	public String create() {
-		return "member/create";
-	}
-
-	@RequestMapping(value = "/detail", method = RequestMethod.GET)
-	public String detail() {
-		return "member/detail";
-	}
-
-	@RequestMapping(value = "/modify", method = RequestMethod.GET)
-	public String modify() {
-		return "member/modify";
+	@RequestMapping(value = "/{viewName}", method = RequestMethod.GET)
+	public String view(@PathVariable String viewName) {
+		return "member/" + viewName;
 	}
 
 }
